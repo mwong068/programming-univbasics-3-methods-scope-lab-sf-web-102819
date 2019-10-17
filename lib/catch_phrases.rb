248976916c433a94409a9a014a52a3ccfc690079
Megan Wong <mwong068@gmail.com>
2019-10-17 03:58:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  line = "It's Dangerous To Go Alone! Take This."
+  puts line
+end
+
+def any_phrase
+  star_fox = "Do A Barrel Roll!"
+  puts star_fox
+end
